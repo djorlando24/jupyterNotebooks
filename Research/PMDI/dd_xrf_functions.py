4805dc48096f03c6f6a83697514c2c1ebd1f070e
Daniel Duke <daniel.duke@monash.edu>
2023-03-14 23:52:49 +1100

    signal trapping functions updated

---
+++ Research/PMDI/dd_xrf_functions.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+    X-RAY FLUORESCENCE SIGNAL TRAPPING CORRECTION FUNCTIONS
+    from PMDICanister_02_signalTrappingModel.ipynb
+    
+    @author Daniel Duke <daniel.duke@monash.edu>
+    @copyright (c) 2022 LTRAC
+    @license GPL-3.0+
+    @version 0.0.1
+    @date 14/03/2023
+        __   ____________    ___    ______
+       / /  /_  ____ __  \  /   |  / ____/
+      / /    / /   / /_/ / / /| | / /
+     / /___ / /   / _, _/ / ___ |/ /_________
+    /_____//_/   /_/ |__\/_/  |_|\__________/
+
+    Laboratory for Turbulence Research in Aerospace & Combustion (LTRAC)
+    Monash University, Australia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Version history:
+        26/02/2023 - First version.
+        14/03/2023 - Updated to load required libraries and tested ok.
+"""
+
+import numpy as np
+import h5py
+import scipy.optimize, scipy.integrate, scipy.interpolate
+
+def mergeInterpolateScans(xData, yData, zData, positionDecimalPlaces=3):
+    '''
+        Take a set of HDF objects and stitch together using linear interpolation
+        onto a rectilinear grid that can easily be plotted and analysed.
+        'positionDecimalPlaces' arg will round off the position locations in case
+        of small variations between scans.
+    '''
+    
+    # Pull all data into flat lists.
+    xAll = []; yAll = []; zAll = []
+    for k in xData:
+        xAll.extend(xData[k][...].ravel())
+        yAll.extend(yData[k][...].ravel())
+        zAll.extend(zData[k][...].ravel())
+        
+    # Find unique values for x and y across all data sets to build points to interpolate onto.
+    xp = np.unique(np.round(xAll,positionDecimalPlaces))
+    yp = np.unique(np.round(yAll,positionDecimalPlaces))
+    x, y = np.meshgrid(xp, yp)
+    
+    # Interpolation of z
+    L1=scipy.interpolate.LinearNDInterpolator([(xAll[n], yAll[n]) for n in range(len(xAll))], zAll)
+    
+    pts = [(x.ravel()[n], y.ravel()[n]) for n in np.arange(np.product(x.shape))]
+    z = L1(pts).reshape(x.shape)
+    
+    # Strictly mask points that are not just outside the convex hull, but
+    # outside the rectilinear grid defined by each scan.
+    mask = np.zeros_like(z)
+    for k in xData:
+        xRange = (np.nanmin(xData[k]), np.nanmax(xData[k]))
+        yRange = (np.nanmin(yData[k]), np.nanmax(yData[k]))
+        mask[(x>=xRange[0])&(x<=xRange[1])&(y>=yRange[0])&(y<=yRange[1])] += 1
+    z[mask==0] = np.nan
+    
+    # Remove completely empty rows and columns of the array
+    nonEmpty = np.max(mask,axis=1)>0
+    x = x[nonEmpty,...]
+    y = y[nonEmpty,...]
+    z = z[nonEmpty,...]
+    nonEmpty = np.max(mask,axis=0)>0
+    x = x[:,nonEmpty,...]
+    y = y[:,nonEmpty,...]
+    z = z[:,nonEmpty,...]
+
+    return x,y,z
+
+def readHorizontalScans(xData, yData, tData, zData, positionDecimalPlaces=3, timeDecimalPlaces=0):
+    '''
+        Take a set of HDF objects as repeated scans in time and output for horizontal scans a 2D matrix
+        of (x,y,time).
+    '''
+    
+    xAll = []; yAll = []; tAll = []; zAll = []
+    for k in xData:
+        xAll.extend(xData[k][...].ravel())
+        yAll.extend(yData[k][...].ravel())
+        tAll.extend(tData[k][...].ravel())
+        zAll.extend(zData[k][...].ravel())
+    
+    # Find unique values for x and y across all data sets.
+    xp = np.unique(np.round(xAll,positionDecimalPlaces))
+    yp = np.unique(np.round(yAll,positionDecimalPlaces))
+    tp = np.unique(np.round(tAll,timeDecimalPlaces))
+
+    x, y, t = np.meshgrid(xp, yp, tp, indexing='ij')
+    
+    # We will fill the result matrix using nearest-neighbour Delaunay triangulation
+    L1=scipy.interpolate.NearestNDInterpolator([(xAll[n], yAll[n], tAll[n]) for n in range(len(xAll))], zAll)
+    
+    pts = [(x.ravel()[n], y.ravel()[n], t.ravel()[n]) for n in np.arange(np.product(x.shape))]
+    z = L1(pts).reshape(x.shape)
+    
+    
+    # Strictly mask points that are not just outside the convex hull, but
+    # outside the rectilinear grid defined by each scan.
+    mask = np.zeros_like(z); tol=1e-3
+    for k in xData:
+        xRange = (np.nanmin(xData[k])-tol, np.nanmax(xData[k])+tol)
+        yRange = (np.nanmin(yData[k])-tol, np.nanmax(yData[k])+tol)
+        tRange = (np.nanmin(tData[k])-tol, np.nanmax(tData[k])+tol)
+        xTest = (x>=xRange[0])&(x<=xRange[1])
+        yTest = (y>=yRange[0])&(y<=yRange[1]) # skip exact y check for horizontal scans
+        tTest = (t>=tRange[0])&(t<=tRange[1])
+        mask[xTest&yTest&tTest] += 1
+    z[mask==0] = np.nan
+    
+    return x,y,t,z
+
+def secant(x,r=1.0):
+    ''' 
+        Secant of a circle radius r at distance x from the centerline.
+        Returns zero when outside the radius.
+    '''
+    s = np.zeros_like(x)
+    s[np.abs(x)<=r] = 2*np.sqrt(r**2 - x[np.abs(x)<=r]**2)
+    return s
+
+def secantAnnulus(x,ri,ro):
+    '''
+        Secant of an annulus with inner radius ri and outer ro, at
+        distance x from the centerline.
+        Returns zero when outside the radius
+    '''
+    s = secant(x,ro)
+    s -= secant(x,ri)
+    return s
+
+def secantPartialInsideCircle(x,z,R,rDet,thetaDet):
+    '''
+        Find the length of a segment of a secant from point (x,z) inside a circle of radius R
+        to a detector outside the circle at radius rDet and angle thetaDet from the origin.
+        
+        This function can accept 1D vectors for x and z.
+    '''
+    rA = np.sqrt(x**2 + z**2)
+    
+    sd = np.sqrt ( (rDet*np.cos(thetaDet) - x)**2 + (rDet*np.sin(thetaDet) - z)**2 )
+    sdQ= np.sqrt ( (rDet*np.cos(thetaDet) + x)**2 + (rDet*np.sin(thetaDet) + z)**2 )    
+
+    cosGamma = (-rDet**2 + x**2 + z**2 + sdQ**2)/(2*sdQ*rA)
+    qb = 2*cosGamma*rA
+    qc = x**2 + z**2 - R**2
+
+    
+    # solve gfsq avoiding imaginary solutions
+    de = (qb**2)/4 - qc
+    sdi = (-qb/2 + np.sqrt(np.abs(de))) * (de>=0) 
+    #sdi2 = (-qb/2 - np.sqrt(np.abs(de))) * (de>=0) 
+    
+    #sdi = np.nanmax(np.vstack((sdi1,sdi2)),axis=0)
+    
+    # no negative solutions allowed
+    sd[sd<0]=0
+    sdi[sdi<0]=0 
+    
+    # zero solutions outside the circle
+    sdi[rA>R]=0
+    
+    return sd, sdi
+
+def secantRayProjector(x,z,R,rDet,thetaDet,rayResolutionPts=150):
+    '''
+        For points outside the circle, project a ray and determine the path length of secant that
+        passes through the circle. int rayResolutionPts set the number of points along each ray.
+        
+        This function can accept 1D vectors for x and z.
+    '''
+    # find ray from point x,z to detector and check if it passes thru the circle
+    zDet = rDet*np.sin(thetaDet)
+    xDet = rDet*np.cos(thetaDet)
+    m = (rDet*np.sin(thetaDet) - z)/(rDet*np.cos(thetaDet) - x)
+    c = z - m*x
+    xp = np.linspace(x,xDet,rayResolutionPts)
+    zp = m*xp + c
+    rp = np.sqrt(xp**2 + zp**2)
+    rayData = [xp,zp,rp<=R]
+    
+    sd,sdi = secantPartialInsideCircle(xp,zp,R,rDet,thetaDet)
+    
+    # set points outside circle with rays passing thru circle to the max value along the ray.
+    rA = np.sqrt(x**2 + z**2)
+    outsideCircle = rA>R
+    sdi_outside = np.zeros_like(x)
+    #print(sdi_outside.shape, sdi.shape)
+    sdi_outside[outsideCircle] = np.nanmax(sdi[:,outsideCircle],axis=0)
+    
+    return sdi_outside, rayData
+    
+def secantPathLengthWrapper(x,z,R,rDet,thetaDet,rayResolutionPts=150):
+    ''' 
+        A wrapper function to compute the internal and external part of the projected path length
+        and add them together. Other returned values are passed through.
+        
+        This function can accept 1D vectors for x and z.
+    '''
+    sd,sdi=secantPartialInsideCircle(x,z,R,rDet,thetaDet)
+    sdi_outside,rayData=secantRayProjector(x,z,R,rDet,thetaDet,rayResolutionPts)
+    return sdi+sdi_outside, sd, rayData
+
+def calcR2(f, xdata, ydata, popt):
+    residuals = ydata- f(xdata, *popt)
+    ss_tot = np.sum((ydata-np.mean(ydata))**2)
+    return 1 - (np.nansum(residuals**2) / ss_tot)
+
+
+################################################################################################################
+if __name__ == '__main__':
+    print("This file is intended to be loaded as a module from iPython notebook")
---
+++ Research/PMDI/dd_xrf_sigtrap.py
@@ -0,0 +1,173 @@
+"""
+    X-RAY FLUORESCENCE SIGNAL TRAPPING CORRECTION FUNCTIONS
+    from PMDICanister_03_signalTrappingApply.ipynb,
+    for re-use on multiple files and scans.
+    
+    @author Daniel Duke <daniel.duke@monash.edu>
+    @copyright (c) 2022 LTRAC
+    @license GPL-3.0+
+    @version 0.0.1
+    @date 14/03/2023
+        __   ____________    ___    ______
+       / /  /_  ____ __  \  /   |  / ____/
+      / /    / /   / /_/ / / /| | / /
+     / /___ / /   / _, _/ / ___ |/ /_________
+    /_____//_/   /_/ |__\/_/  |_|\__________/
+
+    Laboratory for Turbulence Research in Aerospace & Combustion (LTRAC)
+    Monash University, Australia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Version history:
+        14/03/2023 - First version.
+"""
+
+from dd_xrf_functions import *
+import numpy as np
+import h5py
+
+def applySignalTrapping(x_All,y_All,fluor_All,pin_All,I0_All,\
+                        propellant = '134',ethMassFrac=0.08, tracer='I', line='a'):
+    '''
+        Apply signal trapping corrections in the x,z plane. Assuming a cylindrical canister and uniform external environment
+        with a uniform internal liquid density (but nonuniform fluorescing tracer concentration possible).
+        
+        Can handle n-dimensional array data by flattening everything and assessing each sample one at a time.
+    '''
+        
+    # Constants determined by fitting to empty canister data.
+    x0=0                 # x-alignment of center of the canister.
+    thkCan=0.706982      # canister wall thickness [mm]
+    muCan=1.732022       # attenuation/rho of the canister wall at incident beam energy [1/cm]
+    muExt=0.455318       # attenuation/rho of the external components 
+    
+    # Functions that depend on y.
+    thkExtGradient = lambda y: 0.2   # attenuation gradient in x of the external environment [1/cm/mm]
+    thkExtConst = lambda y: 75.      # attenuation average of the external environment [1/cm]
+    ri = lambda y: 10.14               # canister inner wall radius
+    riValve = lambda y: 0.           # effective radius of the metering valve or canister dome part [mm]
+    
+    # X-ray constants
+    
+    # attenuation/rho of the liquid formulation at the incident energy [1/cm]
+    muFluid = {'134':0.328336,'152':0.222837,'1234E':0.271999,'EtOH':0.186543}   
+    
+    # attenuation/rho of the fluorescing tracer at the incident energy [1/cm]
+    muTracer  = {'I':19.4738,'Ba':12.933*137.327/233.38}
+    
+    # attenuation/rho of the liquid formulation at the emission energy [1/cm]
+    muFluid_Ka  = {'I':{'134':0.572199,'152':0.364844,'1234E':0.464389,'EtOH':0.330578},\
+                   'Ba':{'134':0.464644,'152':0.30246,'1234E':0.379642,'EtOH':0.289781}}
+    
+    # attenuation/rho of the canister wall at the emission energy [1/cm]
+    muCan_Ka  = {'I':muCan * 3.4816/1.38491,'Ba':muCan * 2.54821/1.38491}
+    
+    # attenuation/rho of the air at the emission energy [1/cm]
+    muAir_Ka  = {'I':0.0004244,'Ba':0.000359733}
+    
+    # fraction of emission at the chosen K emission line for analysis (a=alpha, b=beta)
+    lineYield = {'a':1.54, 'b':0.18} # very similar for both I and Ba
+    
+    fluorYield = 0.85 # fluorescence yield for this edge.  Very similar for both I and Ba
+    
+    # Detector constants
+    rDet = 275.                        # distance from canister axis to the detector
+    thetaDet = 67*np.pi/180.           # angle of the detector with respect to the X axis of the scan.
+    thetaDet += 90.
+    solidDet = np.pi*(15**2)/(rDet**2) # approximate solid angle of the detector relative to canister axis.
+    
+    # Calculation fluid mixture absorption, accounting for any density differences.
+    muFluidMix = muFluid[propellant]*(1-ethMassFrac) + muFluid['EtOH']*ethMassFrac
+    muFluidMix_emission = muFluid_Ka[tracer][propellant]*(1-ethMassFrac) +\
+                          muFluid_Ka[tracer]['EtOH']*ethMassFrac
+    
+    
+    # Setup arrays
+    x = x_All.ravel() - x0 # shift x positions so canister is in the centre of the coordinate system.
+    y = y_All.ravel()
+    f = fluor_All.ravel()
+    p = pin_All.ravel()
+    I0 = I0_All.ravel()
+    correction = np.ones_like(x)
+    
+    # Loop each element in the matrix
+    for i in range(len(x)):
+        
+        # Determine attenuation lengths of incident beam in ext & wall, assuming z-symmetry.
+        incidentAbsModel = canisterWallExtFn(x[i],0,ri(y[i]),thkCan,muCan, thkExtGradient(y[i]),\
+                                            thkExtConst(y[i]), muExt, riValve(y[i]))
+        
+        # Determine path length of incident beam thru the fluid in the can. 
+        # Missing step here: Assume full instead of using Pin to check full/empty!
+        fluidPathLength = secant(x[i],r=ri(y[i]))
+    
+        # Add the absorption due to the liquid through secant.
+        beamAttenuation = incidentAbsModel + fluidPathLength*muFluidMix*0.1
+        # 'beamAttenuation' should now be close to the Pin Diode -log(I/I0) value.
+
+        # Take half the incident attenuation and calculate the flux at z=0 by accounting for I0 variation.
+        incidentFluxVariation = I0[i]/np.nanmean(I0)
+        beamAbsorptionActual = 1 - (p[i]/I0[i])
+        fluxAtFocus = np.exp(-beamAttenuation*0.5) * beamAbsorptionActual * incidentFluxVariation
+        
+        # Determine amount of emitted radiation in the solid angle, assuming the tracer is present
+        possibleEmission = lineYield[line] * fluorYield * solidDet * fluxAtFocus \
+                           / sum(list(lineYield.values()))
+        
+        # Determine attenuation of emitted radiation through the liquid to the wall.
+        # Sample several positions either side of z=0 to account for contributions along the beam path.
+        z_ = np.linspace(-2.5,2.5,5) # mm
+        s1,s2,rayData = secantPathLengthWrapper(x[i]*np.ones_like(z_),z_,ri(y[i])*np.ones_like(z_),rDet,thetaDet)
+        attenLengthEmission_fluid = np.nanmean(s1)*muFluidMix_emission*0.1
+        
+        # Determine attenuation of emitted radiation through the wall and exterior and air.
+        attenLengthEmission_can = thkCan*muCan_Ka[tracer]*0.1
+        attenLengthEmission_air = np.nanmean(s2)*muAir_Ka[tracer]*0.1
+        
+        # Generate the lumped constant for the signal trapping correction for all above effects:
+        # attenuation of the emission & amount of flux absorbed at the focus.
+        correction[i]=np.exp(- attenLengthEmission_fluid - attenLengthEmission_can - attenLengthEmission_air )
+        correction[i]*=fluxAtFocus
+    
+    return correction.reshape(fluor_All.shape)
+
+
+def readFluorescence(filename, fluorLine='I Ka'):
+    with h5py.File(filename,'r') as H:
+        
+        # Read data.
+        x,y,t,zF = readHorizontalScans( H['HorizontalScans/x'], H['HorizontalScans/y'],\
+                                        H['HorizontalScans/shotCounter'],\
+                                        H['HorizontalScans/%s/integral' % fluorLine] )
+        
+        x,y,t,zP = readHorizontalScans( H['HorizontalScans/x'], H['HorizontalScans/y'],\
+                                        H['HorizontalScans/shotCounter'],\
+                                        H['HorizontalScans/pinDiode'] )
+
+        x,y,t,I0 = readHorizontalScans( H['HorizontalScans/x'], H['HorizontalScans/y'],\
+                                        H['HorizontalScans/shotCounter'],\
+                                        H['HorizontalScans/diamondMonitor'] )
+
+        # Calculations
+        pinDiode = -np.log(zP/I0)
+        
+        # Outlier masking
+        for i in range(x.shape[1]):
+            for j in range(x.shape[2]):
+                dz = np.diff(zF[...,i,j])
+                dz = np.hstack((0, dz))
+                zF[np.abs(dz)>100,i,j] = np.nan # mask large sudden changes in level
+
+    return x,y,t,zP,I0,zF
